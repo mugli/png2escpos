0fd7d6c26c5e59b432360fe57e07f7b3a199fe68
Mehdi Hasan <mhasan@omicronlab.com>
2019-07-26 21:57:21 +0600

    Update usage

---
+++ README.md
@@ -0,0 +1,51 @@
+# png2escpos
+
+Quickly convert PNG files to ESC/POS format, for printing on Epson thermal point-of-sale printers.
+
+This utility removes transparency from image, makes it grayscale and then encodes in ESC/POS format.
+
+---
+
+## Installation
+
+Download binary from [release tab](https://github.com/mugli/png2escpos/releases).
+
+---
+
+## Usage
+
+```
+./png2escpos <filename.png>:
+	Binary output in ESC/POS format will be written directly to stdout.
+
+	You can pipe this output directly into an Epson compatible thermal printer with:
+		Linux:	./png2escpos <file.png> > /dev/usb/lp0
+		macOS:	./png2escpos <file.png> | lpr -P NAME_OF_PRINTER
+
+		Or, if you have a network printer listening at 192.168.1.100:9100
+			you can use socat to forward from stdin to network like this:
+
+			Linux:	./png2escpos <file.png> > socat STDIN TCP4:192.168.1.100:9100
+			macOS:	./png2escpos <file.png> | socat STDIN TCP4:192.168.1.100:9100
+
+			(you may have to install socat on macOS with brew install socat first)
+
+Other commands:
+
+	help, --help, -h:
+		Shows this message
+```
+
+---
+
+## Acknowledgement
+
+[NielsLeenheer/EscPosEncoder](https://github.com/NielsLeenheer/EscPosEncoder) - For the rasterization and ESC/POS encoding in JavaScript.
+
+[twg/png2escpos](https://github.com/twg/png2escpos) - For CLI usage pattern.
+
+---
+
+## License
+
+MIT
--- png2escpos.go
+++ png2escpos.go
@@ -12,6 +12,10 @@ func main() {
 		showHelp()
 	}
 
+	if os.Args[1] == "help" || os.Args[1] == "--help" || os.Args[1] == "-h" {
+		showHelp()
+	}
+
 	imgFile := os.Args[1]
 	err := escpos.PrintImage(imgFile, os.Stdout)
 
@@ -22,5 +26,52 @@ func main() {
 }
 
 func showHelp() {
-	os.Exit(1)
+	help := `
+
+
+	▄▄▄▄▄▄▄ ▄▄  ▄ ▄▄▄▄▄▄▄ 
+	█ ▄▄▄ █ ▄▀▄ █ █ ▄▄▄ █ 
+	█ ███ █ █▄▄▀  █ ███ █ 
+	█▄▄▄▄▄█ ▄ ▄ ▄ █▄▄▄▄▄█ 
+	▄▄▄▄  ▄ ▄▀█  ▄  ▄▄▄ ▄ 
+	▀▄█ ▀▀▄▀ ▀▀ █ ▄▄▀█▄▄█ 
+	███▄▄▄▄▄█ █▄▀ ▄█ ▀ ▀  
+	▄▄▄▄▄▄▄ ▀▄▄  ▀▄▄█▄▄▀  
+	█ ▄▄▄ █  ▄▀█▀█  ▀ █ ▀ 
+	█ ███ █ █▄█▄ █▄▀   ▀  
+	█▄▄▄▄▄█ █▄▄██▄▀█▄ ▄ ▀ 
+
+
+
+------
+Usage:
+
+Quickly converts PNG files to ESC/POS format and write to stdout, 
+	for printing on Epson thermal point-of-sale printers.
+
+This utility removes transparency from image, makes it grayscale
+and then encodes in ESC/POS format.
+
+./png2escpos <filename.png>:
+	Binary output in ESC/POS format will be written directly to stdout.
+
+	You can pipe this output directly into an Epson compatible thermal printer with:
+		Linux:	./png2escpos <file.png> > /dev/usb/lp0
+		macOS:	./png2escpos <file.png> | lpr -P NAME_OF_PRINTER
+
+		Or, if you have a network printer listening at 192.168.1.100:9100 
+			you can use socat to forward from stdin to network like this:
+			
+			Linux:	./png2escpos <file.png> > socat STDIN TCP4:192.168.1.100:9100
+			macOS:	./png2escpos <file.png> | socat STDIN TCP4:192.168.1.100:9100
+			
+			(you may have to install socat on macOS with brew install socat first)
+		
+Other commands:
+
+	help, --help, -h:	
+		Shows this message
+`
+	fmt.Println(help)
+	os.Exit(0)
 }
